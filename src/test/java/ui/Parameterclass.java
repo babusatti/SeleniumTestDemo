66aca5bbe91c0ce89c640d5a2b23f94134d3f1c8
babusatti <33149941+babusatti@users.noreply.github.com>
2021-07-28 18:51:22 +0530

    Update Parameterclass.java

--- src/test/java/ui/Parameterclass.java
+++ src/test/java/ui/Parameterclass.java
@@ -58,7 +58,7 @@ public class Parameterclass {
 		//login.click();			
        		 System.out.println("Login Done with Click");
 		try {
-			Thread.sleep(900);
+			Thread.sleep(1500);
 		} catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
